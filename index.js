e03e02ab3bdcb4b65237c861fd8aee3e6d1a8741
KomfortKimko <komfortkimk@gmail.com>
2024-06-27 06:52:10 +0300

    Functions lab 2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "awesome") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
